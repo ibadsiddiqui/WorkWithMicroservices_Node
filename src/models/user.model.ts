66c5ae03c7dcefe2bfb9c14a61fa024b43edfaed
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2020-03-05 17:11:22 +0500

    --Commit: user model added

---
+++ src/models/user.model.ts
@@ -0,0 +1,45 @@
+import { Entity, model, property } from '@loopback/repository';
+
+@model()
+export class User extends Entity {
+  @property({
+    type: 'string',
+    required: true,
+  })
+  first_name: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  last_name: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  date_of_birth: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  password: string;
+
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
+
+export interface UserRelations {
+  // describe navigational properties here
+}
+
+export type UserWithRelations = User & UserRelations;
